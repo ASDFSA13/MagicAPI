6383b31cfb9ccb10834b002d1b7dc5844b231b77
zyl <big.lu@foxmail.com>
2025-02-25 08:40:44 +0800

    swagger2文档的位置

--- src/main/java/org/ssssssss/example/MagicAPIExampleApplication.java
+++ src/main/java/org/ssssssss/example/MagicAPIExampleApplication.java
@@ -2,11 +2,11 @@ package org.ssssssss.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-//@EnableSwagger2	// 配置swagger 文档
+@EnableSwagger2	// 配置swagger 文档 http://192.168.9.242:9999/v2/api-docs/magic-api/swagger2.json
 public class MagicAPIExampleApplication {
 
 	public static void main(String[] args) {
